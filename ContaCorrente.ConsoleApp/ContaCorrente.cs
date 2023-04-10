41b869d17dbdf7cf1942e289f7d69b14072dc2ac
Artur Ribeiro Alfa <phoenixrebirth1324@gmail.com>
2023-04-10 14:35:56 -0300

    Add project files.

---
+++ ContaCorrente.ConsoleApp/ContaCorrente.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+
+namespace ContaCorrente.ConsoleApp
+{
+    internal class ContaCorrente
+    {
+        private int saldo;
+        private int numero;
+        private int limite;
+        private bool ehEspecial;
+        private ArrayList movimentacoes;
+
+
+
+        public ContaCorrente(int saldo, int numero, int limite, bool ehEspecial, ArrayList movimentacoes)
+        {
+            this.saldo = saldo;
+            this.numero = numero;
+            this.limite = limite;
+            this.ehEspecial = ehEspecial;
+            this.movimentacoes = movimentacoes;
+        }
+
+        public void Sacar(int valor)
+        {
+            if (limite + saldo > valor)
+            {
+                this.saldo -= valor;
+                this.movimentacoes.Add($"Saque no valor de {valor}");
+            }
+            else
+                Console.WriteLine("Valor de saque maior que saldo mais limite");
+        }
+
+        public void Depositar(int valor)
+        {
+            this.saldo += valor;
+            this.movimentacoes.Add($"Deposito no valor de {valor}");
+        }
+
+        public void VisualizarSaldo()
+        {
+            Console.WriteLine("Saldo: "+this.saldo);
+        }
+
+        public void VisualizarExtrato()
+        {
+            Console.WriteLine("Saldo: "+this.saldo);
+            Console.WriteLine("Número da Conta: "+this.numero);
+            Console.WriteLine("Limite da Conta: "+this.limite);
+            Console.WriteLine("É especial: "+this.ehEspecial);
+            Console.WriteLine("Movimentações");
+            foreach (string movimentacao in this.movimentacoes)
+            {
+                Console.WriteLine(movimentacao);
+            }
+        }
+
+        public void Transferencia(ContaCorrente conta, int valor)
+        {
+            if (limite + saldo > valor)
+            {
+                this.Sacar(valor);
+                conta.Depositar(valor);
+            }
+            else
+                Console.WriteLine("Valor de transferencia maior que saldo mais limite");
+        }
+    }
+}
---
+++ ContaCorrente.ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+namespace ContaCorrente.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta1 = new ContaCorrente(1000, 12, 0, true, new ArrayList());
+            conta1.Sacar(200);
+            conta1.Depositar(300);
+            conta1.Depositar(500);
+            conta1.Sacar(200);
+
+            ContaCorrente conta2 = new ContaCorrente(300, 13, 0, true, new ArrayList());
+
+            conta1.Transferencia(conta2, 400);
+            conta1.VisualizarExtrato();
+
+            Console.ReadKey();
+        }
+    }
+}
